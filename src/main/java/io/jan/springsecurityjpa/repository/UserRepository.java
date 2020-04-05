11be467fd6b1b3e7446658814b39b438129c813b
Sajan Sthapit <sth.saj99@gmail.com>
2020-04-05 19:37:31 +0545

    -jpa security added

---
+++ src/main/java/io/jan/springsecurityjpa/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package io.jan.springsecurityjpa.repository;
+
+import io.jan.springsecurityjpa.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUserName(String userName);
+}
