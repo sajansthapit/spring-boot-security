7ee689781197de25a6914e7999f5f4082da885d6
Sajan Sthapit <sth.saj99@gmail.com>
2020-04-05 19:09:08 +0545

    basic security configured

---
+++ src/main/java/io/jan/springsecurityjpa/resources/HomeResource.java
@@ -0,0 +1,23 @@
+package io.jan.springsecurityjpa.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String home(){
+        return "<h1>Hello Users</h1>";
+    }
+
+    @GetMapping("/users")
+    public String user(){
+        return "<h1>Welcome User</h1>";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "<h1>Welcome Admin</h1>";
+    }
+}
